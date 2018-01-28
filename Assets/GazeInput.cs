fd8535bd579dd215c57a747012c349d22fb28694
Zachary Geier <zdgeier@gmail.com>
2018-01-28 02:04:24 -0500

    Initial Commit

---
+++ Assets/GazeInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeInput : MonoBehaviour {
+
+    public GameObject map;
+
+	// Use this for initialization
+	void Start () {
+        QualitySettings.antiAliasing = 4;
+        OVRTouchpad.Create();
+        OVRTouchpad.TouchHandler += HandleTouchHandler;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp("h"))
+        {
+            hideBuildings();
+        }
+	}
+
+    void HandleTouchHandler(object sender, System.EventArgs e)
+    {
+        OVRTouchpad.TouchArgs touchArgs = (OVRTouchpad.TouchArgs)e;
+        if (touchArgs.TouchType == OVRTouchpad.TouchEvent.SingleTap)
+        {
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, fwd, out hit))
+                hit.transform.SendMessage("HitByRay");
+        }
+
+        if (touchArgs.TouchType == OVRTouchpad.TouchEvent.Up)
+        {
+            hideBuildings();
+        }
+    }
+
+    private void hideBuildings()
+    {
+        for (int i = 0; i < map.transform.childCount; i++) {
+            bool active = map.transform.GetChild(i).GetChild(0).gameObject.activeSelf;
+            map.transform.GetChild(i).GetChild(0).gameObject.SetActive(!active);
+        }
+    }
+}
---
+++ Assets/OnRayHitShowInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnRayHitShowInfo : MonoBehaviour {
+    private Transform child;
+
+    private void Start()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(true);
+    }
+
+    void HitByRay()
+    {
+        bool isActive = transform.GetChild(0).gameObject.activeSelf;
+        transform.GetChild(0).gameObject.SetActive(!isActive);
+        transform.GetChild(1).gameObject.SetActive(isActive);
+    }
+}
